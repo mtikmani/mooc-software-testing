e92904e18c7d379460cd4ddae9f66c8842cc1e96
mtikmani <mtikmani@gmail.com>
2018-11-09 09:33:47 +0530

    completing add 5 challenge

--- src/test/java/tudelft/roman/RomanNumeralTestWithBeforeEach.java
+++ src/test/java/tudelft/roman/RomanNumeralTestWithBeforeEach.java
@@ -1,8 +1,9 @@
 package tudelft.roman;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RomanNumeralTestWithBeforeEach {
 
@@ -16,24 +17,24 @@ public class RomanNumeralTestWithBeforeEach {
     @Test
     public void singleNumber() {
         int result = roman.convert("I");
-        Assertions.assertEquals(1, result);
+        assertEquals(1, result);
     }
 
     @Test
     public void numberWithManyDigits() {
         int result = roman.convert("VIII");
-        Assertions.assertEquals(8, result);
+        assertEquals(8, result);
     }
 
     @Test
     public void numberWithSubtractiveNotation() {
         int result = roman.convert("IV");
-        Assertions.assertEquals(4, result);
+        assertEquals(4, result);
     }
 
     @Test
     public void numberWithAndWithoutSubtractiveNotation() {
-        int result = roman.convert("XLIV");
-        Assertions.assertEquals(44, result);
+        int result = roman.convert("XXXXLIV");
+        assertEquals(74, result);
     }
 }
